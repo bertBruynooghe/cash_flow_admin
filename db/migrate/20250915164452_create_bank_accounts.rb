d67fb2c13fe4e91ea2c553f001cde3d1822dd539
Bert Bruynooghe <bert.bruynooghe@gmail.com>
2025-09-15 18:45:31 +0200

    addded bank_account model

---
+++ db/migrate/20250915164452_create_bank_accounts.rb
@@ -0,0 +1,10 @@
+class CreateBankAccounts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :bank_accounts do |t|
+      t.string :name
+      t.string :account_number
+
+      t.timestamps
+    end
+  end
+end
